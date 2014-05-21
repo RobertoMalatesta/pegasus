d6577cdf68530af0130a9af4487b64496bb73426
Andrew Page <andrew@andrewpage.me>
2014-05-21 15:39:37 -0700

    We are now using KML to display country boundaries and shading

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -2,8 +2,16 @@ function initialize() {
     var mapOptions = {
             center: new google.maps.LatLng(15.697718, 16.486035),
             mapTypeId: google.maps.MapTypeId.SATELLITE,
-            zoom: 3,
-            disableDefaultUI: true
+            disableDefaultUI: true,
+            zoom: 3
           };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"));
+
+    var layer = new google.maps.KmlLayer({
+            url: 'https://s3-us-west-1.amazonaws.com/pegasus-mun/doc.kml',
+            map: map,
+            preserveViewport: true
+          });
+
+    map.setOptions(mapOptions);
 };
