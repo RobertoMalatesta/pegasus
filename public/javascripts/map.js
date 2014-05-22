18fb673e102b07276150add36d418a7593f2eaa9
Andrew Page <andrew@andrewpage.me>
2014-05-21 21:25:32 -0700

    Live update map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,6 @@
 function initialize() {
+    var mapUrl = randUrl();
+    
     var mapOptions = {
             center: new google.maps.LatLng(15.697718, 16.486035),
             mapTypeId: google.maps.MapTypeId.SATELLITE,
@@ -13,12 +15,6 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var layer = new google.maps.KmlLayer({
-            url: 'https://s3-us-west-1.amazonaws.com/pegasus-mun/doc.kml',
-            map: map,
-            preserveViewport: true
-          });
-
     var allowedBounds = new google.maps.LatLngBounds(
        new google.maps.LatLng(70.33956792419954, 178.01171875),
        new google.maps.LatLng(83.86483689701898, -88.033203125)
@@ -27,14 +23,32 @@ function initialize() {
 
     google.maps.event.addListener(map, 'center_changed', function() {
         if (allowedBounds.contains(map.getCenter())) {
-            // still within valid bounds, so save the last valid position
             lastValidCenter = map.getCenter();
             return;
         }
 
-        // not valid anymore => return to last valid position
         map.panTo(lastValidCenter);
     });
 
     map.setOptions(zoomOptions);
+
+    var layer = new google.maps.KmlLayer({
+            url: mapUrl,
+            map: map,
+            preserveViewport: true
+          });
+
+    window.setInterval(refreshKml, 5000, layer, map);
 };
+
+function refreshKml(layer, map) {
+   var newUrl = randUrl();
+
+   layer.setMap(null);
+   layer.setUrl(randUrl());
+   layer.setMap(map);
+}
+
+function randUrl() {
+    return ('https://dl.dropbox.com/s/zcxr2lwkwfnvtgw/content.kml?time=' + (new Date()).getTime());
+}
