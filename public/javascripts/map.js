f9f3b56951b37c6e3d383433f22a782f3aedf3fb
Andrew Page <andrew@andrewpage.me>
2014-05-21 20:15:33 -0700

    Boundaries set

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,7 +5,13 @@ function initialize() {
             disableDefaultUI: true,
             zoom: 3
           };
-    var map = new google.maps.Map(document.getElementById("map"));
+
+    var zoomOptions = {
+        minZoom: 3,
+        maxZoom: 12
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     var layer = new google.maps.KmlLayer({
             url: 'https://s3-us-west-1.amazonaws.com/pegasus-mun/doc.kml',
@@ -13,5 +19,22 @@ function initialize() {
             preserveViewport: true
           });
 
-    map.setOptions(mapOptions);
+    var allowedBounds = new google.maps.LatLngBounds(
+       new google.maps.LatLng(70.33956792419954, 178.01171875),
+       new google.maps.LatLng(83.86483689701898, -88.033203125)
+    );
+    var lastValidCenter = map.getCenter();
+
+    google.maps.event.addListener(map, 'center_changed', function() {
+        if (allowedBounds.contains(map.getCenter())) {
+            // still within valid bounds, so save the last valid position
+            lastValidCenter = map.getCenter();
+            return;
+        }
+
+        // not valid anymore => return to last valid position
+        map.panTo(lastValidCenter);
+    });
+
+    map.setOptions(zoomOptions);
 };
